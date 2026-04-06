49deb9b0246e6a13bce883ceddf1c57431fd88f6
ahmedkalile19-blip <ahmedkalile19@gmail.com>
2026-04-05 22:50:58 -0600

    Light/Dark Theme

---
+++ scripts/theme.js
@@ -0,0 +1,15 @@
+// Select the button
+const toggleBtn = document.querySelector("#theme-toggle");
+
+// Add click event listener to the button
+toggleBtn.addEventListener("click", () => {
+  // Toggle the 'dark' class on the body
+  document.body.classList.toggle("dark");
+
+  // Checkif dark mode is active
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "🌙 Switch to Light Mode";
+  } else {
+    toggleBtn.textContent = "☀️ Switch to Dark Mode";
+  }
+});
